aad74cebb1c414eea6835bb9aa00c90652c39773
Fynn Schulz <fynnschulz@MacBook-Air-von-Fynn.local>
2025-08-08 03:20:38 +0200

    ✅ Supabase fix in protected/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@
  "use client";
 import AuthModal from "./components/AuthModal";
 import { useEffect, useState } from "react";
-import { supabase } from "@/app/lib/supabaseclient";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
